2fbc0ea1f8bbc682488da5b5b4df0af92ab61b67
Joshua Best <112558094+Joshuatbest@users.noreply.github.com>
2022-10-31 09:30:50 -0400

    Add files via upload

---
+++ 0365 subscriptions.ps1
@@ -0,0 +1,16 @@
+﻿#Connect to your office / azureAD account 
+Connect-AzureAD
+
+#look at the skus you have purchased 
+Get-AzureADSubcribedSKu | Select -Property SKu*,ConsumedUnits -ExpandProperty Prepaidunits 
+
+#
+Get-AzureADSubscribedSku | Select SkuPartNumber
+
+
+$licenses = Get-AzureADSubscribedSKu
+
+
+#Shows all of the Service Plan IDs. 
+$licenses[1].ServicePlans
+
